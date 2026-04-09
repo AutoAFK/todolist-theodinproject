081fdfa3a494c5dedf804642c5a2e8e6a78f1a65
AutoAFK <ranhaim85@gmail.com>
2026-04-10 02:32:57 +0300

    Added task manager

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class Task {
    * @param {string} description - The task description
    * @param {TaskStatus[keyof TaskStatus]} status
    */
-  constructor(id = crypto.randomUUID(), description = "", status) {
+  constructor({ id = crypto.randomUUID(), description = "", status }) {
     this.id = id;
     this.description = description;
     this.status = status;
@@ -31,5 +31,40 @@ class Task {
 
   static fromJSON(id, data) {
     return Task(id, data.description, data.status);
+  }
+}
+
+class TaskManager {
+  tasksObj = {};
+
+  constructor() {
+    if (TaskManager.instance) return TaskManager.instance;
+    TaskManager.instance = this;
+  }
+
+  /**
+   * @param {string} description
+   * @param {TaskStatus} status
+   */
+  addTask(description, status) {
+    const task = new Task({ description, status });
+    tasksObj[task.id] = task;
+  }
+
+  removeTask(id) {
+    delete this.tasksObj[id];
+  }
+
+  /**
+   *
+   * @param {*} id
+   * @param {TaskStatus} status
+   */
+  changeStatus(id, status) {
+    this.tasksObj[id].setStatus(status);
+  }
+
+  changeDescription(id, description){
+    this.tasksObj[id].setDescription(description);
   }
 }
