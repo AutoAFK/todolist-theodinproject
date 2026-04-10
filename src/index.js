d0b0c941a256e9e39ea96ccbd7e349f243870316
AutoAFK <ranhaim85@gmail.com>
2026-04-10 20:52:15 +0300

    Render function with filter

--- src/index.js
+++ src/index.js
@@ -69,6 +69,13 @@ class TaskManager {
     this.tasksObj[id].setDescription(description);
   }
 
+  /**
+   * @returns {Array<Task>} - the task objects array
+   */
+  getAllTasksObjects() {
+    return Object.values(this.tasksObj);
+  }
+
   displayAllTasks() {
     for (const task of Object.values(this.tasksObj)) {
       console.table({
@@ -140,3 +147,32 @@ if (testingChangeDescription) {
   );
   manager.displayAllTasks();
 }
+
+/**
+ * 
+ * @param {TaskStatus} filter - status to filter. 
+ */
+function RenderTasks(filter = null) {
+  let tasks = manager.getAllTasksObjects();
+  tasks =
+    filter === null ? tasks : tasks.filter((task) => task.status === filter);
+  const container = document.createElement("div");
+  tasks.forEach((task) => {
+    const taskContainer = document.createElement("div");
+    taskContainer.classList.add("task-container");
+
+    const description = document.createElement("p");
+    description.classList.add("task-description");
+    description.textContent = task.description;
+
+    const status = document.createElement("p");
+    status.classList.add("task-status");
+    status.textContent = task.status;
+
+    taskContainer.append(description, status);
+    container.append(taskContainer);
+  });
+  console.log(container);
+}
+
+RenderTasks();
